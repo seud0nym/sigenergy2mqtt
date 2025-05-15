fea310d5da971b5ea4fe01153946ec84620392cb
Stephen Stevens <seud0nym@yahoo.com.au>
2025-05-15 15:38:34 +1000

    Added override key to exception

--- sigenergy2mqtt/config/config.py
+++ sigenergy2mqtt/config/config.py
@@ -60,61 +60,65 @@ class Config:
         for key, value in os.environ.items():
             if key.startswith("SIGENERGY2MQTT_") and value is not None and value != "None":
                 logging.debug(f"Found env/cli override: {key} = {'******' if 'PASSWORD' in key or 'API_KEY' in key else value}")
-                match key:
-                    case const.SIGENERGY2MQTT_HASS_ENABLED:
-                        overrides["home-assistant"]["enabled"] = check_bool(os.environ[key], key)
-                    case const.SIGENERGY2MQTT_HASS_DISCOVERY_ONLY:
-                        overrides["home-assistant"]["discovery-only"] = check_bool(os.environ[key], key)
-                    case const.SIGENERGY2MQTT_LOG_LEVEL:
-                        overrides["log-level"] = check_log_level(os.environ[key], key)
-                    case const.SIGENERGY2MQTT_MODBUS_HOST:
-                        overrides["modbus"][0]["host"] = check_host(os.environ[key], key)
-                    case const.SIGENERGY2MQTT_MODBUS_PORT:
-                        overrides["modbus"][0]["port"] = check_port(os.environ[key], key)
-                    case const.SIGENERGY2MQTT_MODBUS_INVERTER_SLAVE:
-                        overrides["modbus"][0]["inverters"] = check_int_list([int(slave) for slave in os.environ[key].split(",")], key)
-                    case const.SIGENERGY2MQTT_MODBUS_ACCHARGER_SLAVE:
-                        overrides["modbus"][0]["ac-chargers"] = check_int_list([int(slave) for slave in os.environ[key].split(",")], key)
-                    case const.SIGENERGY2MQTT_MODBUS_DCCHARGER_SLAVE:
-                        overrides["modbus"][0]["dc-chargers"] = check_int_list([int(slave) for slave in os.environ[key].split(",")], key)
-                    case const.SIGENERGY2MQTT_SMARTPORT_ENABLED:
-                        overrides["modbus"][0]["smart-port"]["enabled"] = check_bool(os.environ[key], key)
-                    case const.SIGENERGY2MQTT_SMARTPORT_MODULE_NAME:
-                        overrides["modbus"][0]["smart-port"]["module"]["name"] = os.environ[key]
-                    case const.SIGENERGY2MQTT_SMARTPORT_HOST:
-                        overrides["modbus"][0]["smart-port"]["module"]["host"] = check_host(os.environ[key], key)
-                    case const.SIGENERGY2MQTT_SMARTPORT_USERNAME:
-                        overrides["modbus"][0]["smart-port"]["module"]["username"] = os.environ[key]
-                    case const.SIGENERGY2MQTT_SMARTPORT_PASSWORD:
-                        overrides["modbus"][0]["smart-port"]["module"]["password"] = os.environ[key]
-                    case const.SIGENERGY2MQTT_SMARTPORT_PV_POWER:
-                        overrides["modbus"][0]["smart-port"]["module"]["pv-power"] = os.environ[key]
-                    case const.SIGENERGY2MQTT_SMARTPORT_MQTT_TOPIC:
-                        overrides["modbus"][0]["smart-port"]["mqtt"][0]["topic"] = os.environ[key]
-                    case const.SIGENERGY2MQTT_SMARTPORT_MQTT_GAIN:
-                        overrides["modbus"][0]["smart-port"]["mqtt"][0]["gain"] = check_int(os.environ[key], key, allow_none=True, min=1)
-                    case const.SIGENERGY2MQTT_MQTT_BROKER:
-                        overrides["mqtt"]["broker"] = check_host(os.environ[key], key)
-                    case const.SIGENERGY2MQTT_MQTT_PORT:
-                        overrides["mqtt"]["port"] = check_port(os.environ[key], key)
-                    case const.SIGENERGY2MQTT_MQTT_ANONYMOUS:
-                        overrides["mqtt"]["anonymous"] = check_bool(os.environ[key], key)
-                    case const.SIGENERGY2MQTT_MQTT_USERNAME:
-                        overrides["mqtt"]["username"] = os.environ[key]
-                    case const.SIGENERGY2MQTT_MQTT_PASSWORD:
-                        overrides["mqtt"]["password"] = os.environ[key]
-                    case const.SIGENERGY2MQTT_PVOUTPUT_ENABLED:
-                        overrides["pvoutput"]["enabled"] = check_bool(os.environ[key], key)
-                    case const.SIGENERGY2MQTT_PVOUTPUT_LOG_LEVEL:
-                        overrides["pvoutput"]["log_level"] = check_log_level(os.environ[key], key)
-                    case const.SIGENERGY2MQTT_PVOUTPUT_API_KEY:
-                        overrides["pvoutput"]["api_key"] = check_string(os.environ[key], key, allow_none=False, allow_empty=False, hex_chars_only=True)
-                    case const.SIGENERGY2MQTT_PVOUTPUT_SYSTEM_ID:
-                        overrides["pvoutput"]["system_id"] = check_string(os.environ[key], key, allow_none=False, allow_empty=False)
-                    case const.SIGENERGY2MQTT_PVOUTPUT_CONSUMPTION:
-                        overrides["pvoutput"]["consumption"] = check_bool(os.environ[key], key)
-                    case const.SIGENERGY2MQTT_PVOUTPUT_INTERVAL:
-                        overrides["pvoutput"]["interval_minutes"] = check_int(os.environ[key], key, min=5, max=15)
+                try:
+                    match key:
+                        case const.SIGENERGY2MQTT_HASS_ENABLED:
+                            overrides["home-assistant"]["enabled"] = check_bool(os.environ[key], key)
+                        case const.SIGENERGY2MQTT_HASS_DISCOVERY_ONLY:
+                            overrides["home-assistant"]["discovery-only"] = check_bool(os.environ[key], key)
+                        case const.SIGENERGY2MQTT_LOG_LEVEL:
+                            overrides["log-level"] = check_log_level(os.environ[key], key)
+                        case const.SIGENERGY2MQTT_MODBUS_HOST:
+                            overrides["modbus"][0]["host"] = check_host(os.environ[key], key)
+                        case const.SIGENERGY2MQTT_MODBUS_PORT:
+                            overrides["modbus"][0]["port"] = check_port(os.environ[key], key)
+                        case const.SIGENERGY2MQTT_MODBUS_INVERTER_SLAVE:
+                            overrides["modbus"][0]["inverters"] = check_int_list([int(slave) for slave in os.environ[key].split(",")], key)
+                        case const.SIGENERGY2MQTT_MODBUS_ACCHARGER_SLAVE:
+                            overrides["modbus"][0]["ac-chargers"] = check_int_list([int(slave) for slave in os.environ[key].split(",")], key)
+                        case const.SIGENERGY2MQTT_MODBUS_DCCHARGER_SLAVE:
+                            overrides["modbus"][0]["dc-chargers"] = check_int_list([int(slave) for slave in os.environ[key].split(",")], key)
+                        case const.SIGENERGY2MQTT_SMARTPORT_ENABLED:
+                            overrides["modbus"][0]["smart-port"]["enabled"] = check_bool(os.environ[key], key)
+                        case const.SIGENERGY2MQTT_SMARTPORT_MODULE_NAME:
+                            overrides["modbus"][0]["smart-port"]["module"]["name"] = os.environ[key]
+                        case const.SIGENERGY2MQTT_SMARTPORT_HOST:
+                            overrides["modbus"][0]["smart-port"]["module"]["host"] = check_host(os.environ[key], key)
+                        case const.SIGENERGY2MQTT_SMARTPORT_USERNAME:
+                            overrides["modbus"][0]["smart-port"]["module"]["username"] = os.environ[key]
+                        case const.SIGENERGY2MQTT_SMARTPORT_PASSWORD:
+                            overrides["modbus"][0]["smart-port"]["module"]["password"] = os.environ[key]
+                        case const.SIGENERGY2MQTT_SMARTPORT_PV_POWER:
+                            overrides["modbus"][0]["smart-port"]["module"]["pv-power"] = os.environ[key]
+                        case const.SIGENERGY2MQTT_SMARTPORT_MQTT_TOPIC:
+                            overrides["modbus"][0]["smart-port"]["mqtt"][0]["topic"] = os.environ[key]
+                        case const.SIGENERGY2MQTT_SMARTPORT_MQTT_GAIN:
+                            overrides["modbus"][0]["smart-port"]["mqtt"][0]["gain"] = check_int(os.environ[key], key, allow_none=True, min=1)
+                        case const.SIGENERGY2MQTT_MQTT_BROKER:
+                            overrides["mqtt"]["broker"] = check_host(os.environ[key], key)
+                        case const.SIGENERGY2MQTT_MQTT_PORT:
+                            overrides["mqtt"]["port"] = check_port(os.environ[key], key)
+                        case const.SIGENERGY2MQTT_MQTT_ANONYMOUS:
+                            overrides["mqtt"]["anonymous"] = check_bool(os.environ[key], key)
+                        case const.SIGENERGY2MQTT_MQTT_USERNAME:
+                            overrides["mqtt"]["username"] = os.environ[key]
+                        case const.SIGENERGY2MQTT_MQTT_PASSWORD:
+                            overrides["mqtt"]["password"] = os.environ[key]
+                        case const.SIGENERGY2MQTT_PVOUTPUT_ENABLED:
+                            overrides["pvoutput"]["enabled"] = check_bool(os.environ[key], key)
+                        case const.SIGENERGY2MQTT_PVOUTPUT_LOG_LEVEL:
+                            overrides["pvoutput"]["log_level"] = check_log_level(os.environ[key], key)
+                        case const.SIGENERGY2MQTT_PVOUTPUT_API_KEY:
+                            overrides["pvoutput"]["api_key"] = check_string(os.environ[key], key, allow_none=False, allow_empty=False, hex_chars_only=True)
+                        case const.SIGENERGY2MQTT_PVOUTPUT_SYSTEM_ID:
+                            overrides["pvoutput"]["system_id"] = check_string(os.environ[key], key, allow_none=False, allow_empty=False)
+                        case const.SIGENERGY2MQTT_PVOUTPUT_CONSUMPTION:
+                            overrides["pvoutput"]["consumption"] = check_bool(os.environ[key], key)
+                        case const.SIGENERGY2MQTT_PVOUTPUT_INTERVAL:
+                            overrides["pvoutput"]["interval_minutes"] = check_int(os.environ[key], key, min=5, max=15)
+                except Exception as e:
+                    raise Exception(f"{repr(e)} when processing override '{key}'")
+
         Config._configure(overrides, True)
 
         if len(Config.devices) == 0:
