c99c0a1b70ec6096b2ee0f5e761e1c64cab44c07
Stephen Stevens <seud0nym@yahoo.com.au>
2025-06-11 09:40:16 +1000

    Bump version to 2025.6.11

--- sigenergy2mqtt/config/version.py
+++ sigenergy2mqtt/config/version.py
@@ -1,6 +1,6 @@
 from typing import Final
 
 # Don't use Final on version field because hatchling won't find it if you do
-__version__ = "2025.6.5"
+__version__ = "2025.6.11"
 
 SIGENERGY_MODBUS_PROTOCOL: Final = "V2.5 2025-02-19"
