c111664f77933c20b200aba0de3ed77cb149535d
Stephen Stevens <seud0nym@yahoo.com.au>
2025-07-08 17:14:05 +1000

    Sensor documentation updated

--- sigenergy2mqtt/sensors/doc/update_readme.py
+++ sigenergy2mqtt/sensors/doc/update_readme.py
@@ -7,7 +7,7 @@ from sigenergy2mqtt.devices.types import HybridInverter, PVInverter
 from sigenergy2mqtt.sensors.base import WriteOnlySensor
 
 
-logging.getLogger("root").setLevel(logging.DEBUG)
+logging.getLogger("root").setLevel(logging.INFO)
 
 derived = {
     "BatteryChargingPower": "BatteryPower &gt; 0",
@@ -24,24 +24,15 @@ derived = {
     "GridSensorDailyImportEnergy": "GridSensorLifetimeImportEnergy &minus; GridSensorLifetimeImportEnergy at last midnight",
     "GridSensorExportPower": "GridSensorActivePower &lt; 0 &times; -1",
     "GridSensorImportPower": "GridSensorActivePower &gt; 0",
-    "GridSensorLifetimeExportEnergy": "Riemann &sum; of GridSensorExportPower",
-    "GridSensorLifetimeImportEnergy": "Riemann &sum; of GridSensorImportPower",
     "InverterBatteryChargingPower": "ChargeDischargePower &gt; 0",
     "InverterBatteryDischargingPower": "ChargeDischargePower &lt; 0 &times; -1",
-    "InverterDailyPVEnergy": "InverterLifetimePVEnergy &minus; InverterLifetimePVEnergy at last midnight",
-    "InverterLifetimePVEnergy": "Riemann &sum; of InverterPVPower",
     "PVStringDailyEnergy": "PVStringLifetimeEnergy &minus; PVStringLifetimeEnergy at last midnight",
     "PVStringLifetimeEnergy": "Riemann &sum; of PVStringPower",
     "PVStringPower": "PVVoltageSensor &times; PVCurrentSensor",
-    "PlantAccumulatedChargeEnergy": "&sum; of AccumulatedChargeEnergy across all Inverters associated with the Plant",
-    "PlantAccumulatedDischargeEnergy": "&sum; of AccumulatedDischargeEnergy across all Inverters associated with the Plant",
     "PlantConsumedPower": "TotalPVPower &plus; GridSensorActivePower &minus; BatteryPower",
     "PlantDailyChargeEnergy": "&sum; of DailyChargeEnergy across all Inverters associated with the Plant",
-    "PlantDailyConsumedEnergy": "PlantLifetimeConsumedEnergy &minus; PlantLifetimeConsumedEnergy at last midnight",
     "PlantDailyDischargeEnergy": "&sum; of DailyDischargeEnergy across all Inverters associated with the Plant",
     "PlantDailyPVEnergy": "PlantLifetimePVEnergy &minus; PlantLifetimePVEnergy at last midnight",
-    "PlantLifetimeConsumedEnergy": "Riemann &sum; of PlantConsumedPower",
-    "PlantLifetimePVEnergy": "Riemann &sum; of TotalPVPower",
     "TotalPVPower": "PlantPVPower &plus; (PlantThirdPartyPVPower _or_ &sum; of all configured SmartPort MQTT sources and SmartPort modules)",
     "GeneralPCSAlarm": "Modbus Registers 30027 and 30028",
     "ACChargerAlarms": "Modbus Registers 32012, 32013, and 32014",
@@ -53,17 +44,18 @@ async def sensor_index():
     def published_topics(device):
         f.write("| Sensor Class | Interval | Unit | Gain | State Topic | Source | Applicable To |\n")
         f.write("|--------------|---------:|------|-----:|-------------|--------|---------------|\n")
-        for key in [key for key, value in mqtt_sensors.items() if "state_topic" in value and not isinstance(value, WriteOnlySensor)]:
+        for key in [key for key, value in sorted(mqtt_sensors.items(), key=lambda x: x[1]["name"]) if "state_topic" in value and not isinstance(value, WriteOnlySensor)]:
             sensor = mqtt_sensors[key]
             sensor_parent = None if not hasattr(sensor, "parent_device") else sensor.parent_device.__class__.__name__
-            if sensor_parent == device:
+            if sensor_parent == device and sensor.publishable:
                 sensor_name = sensor.__class__.__name__
-                f.write(f"| {sensor_name} |")
+                f.write(f"| {sensor['name']} |")
                 if hasattr(sensor, "scan_interval"):
                     f.write(f" {sensor.scan_interval}s ")
                 f.write(f"| {'' if sensor.unit is None else sensor.unit} | {'' if sensor.gain is None else sensor.gain} | {sensor.state_topic} <br/> {hass_sensors[key].state_topic} |")
                 if sensor_name in derived:
                     f.write(derived[sensor_name])
+                    del derived[sensor_name]
                 elif hasattr(sensor, "_address"):
                     f.write(f"Modbus Register {sensor._address} ")
                 f.write("|")
@@ -79,12 +71,14 @@ async def sensor_index():
     def subscribed_topics(device):
         f.write("| Sensor Class | Command Topic | Target | Applicable To |\n")
         f.write("|--------------|---------------|--------|---------------|\n")
-        for key in [key for key, value in mqtt_sensors.items() if "command_topic" in value]:
+        for key in [key for key, value in sorted(mqtt_sensors.items(), key=lambda x: x[1]["name"]) if "command_topic" in value]:
             sensor = mqtt_sensors[key]
             sensor_parent = None if not hasattr(sensor, "parent_device") else sensor.parent_device.__class__.__name__
             if sensor_parent == device:
-                sensor_name = sensor.__class__.__name__
-                f.write(f"| {sensor_name} | {sensor.state_topic} <br/> {hass_sensors[key].command_topic} | ")
+                f.write(f"| {sensor['name']} ")
+                if sensor['name'] == "Power":
+                    f.write("On/Off ")
+                f.write(f"| {sensor.state_topic} <br/> {hass_sensors[key].command_topic} | ")
                 if hasattr(sensor, "_address"):
                     f.write(f"Modbus Register {sensor._address} ")
                 f.write("|")
@@ -108,14 +102,17 @@ async def sensor_index():
         f.write("Otherwise, the topics prefixed with `sigenergy2mqtt/` are used.\n")
         f.write("\nThe number after the `sigen_` prefix represents the host index from the configuration file, starting from 0. (Home Assistant configuration may change the `sigen` topic prefix.)")
         f.write("\nInverter, AC Charger and DC Charger indexes use the device address (slave ID) as specified in the configuration file.\n")
-        f.write("\nScan Intervals are shown in seconds. Intervals for derived sensors are dependent on the source sensors.\n")
+        f.write("\nDefault Scan Intervals are shown in seconds, but may be overridden via configuration. Intervals for derived sensors are dependent on the source sensors.\n")
         f.write("\n## Published Topics\n")
         f.write("\n### Plant\n")
         published_topics("PowerPlant")
         f.write("\n#### Grid Sensor\n")
         published_topics("GridSensor")
-        f.write("\n#### Smart-Port\n")
+        f.write("\n#### Smart-Port (Enphase Envoy only)\n")
         published_topics("SmartPort")
+        f.write("\n#### Statistics\n")
+        f.write("\nAfter upgrading the device firmware to support the new Statistics Interface, the register values will reset to 0 and start fresh counting _without_ inheriting historical data.\n")
+        published_topics("PlantStatistics")
         f.write("\n### Inverter\n")
         published_topics("Inverter")
         f.write("\n#### Energy Storage System\n")
@@ -142,3 +139,5 @@ if __name__ == "__main__":
     loop.run_until_complete(sensor_index())
     cancel_sensor_futures()
     loop.close()
+    if derived.keys():
+        logging.getLogger("root").warning(f"Unused derived sensors: {derived.keys()}")
