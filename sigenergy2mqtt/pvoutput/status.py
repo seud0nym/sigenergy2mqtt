44e47e7c03ea3287010932a986faa72f4a85ee4b
Stephen Stevens <seud0nym@yahoo.com.au>
2025-07-16 09:44:18 +1000

    Rolled back power calculations - no added value

--- sigenergy2mqtt/pvoutput/status.py
+++ sigenergy2mqtt/pvoutput/status.py
@@ -1,11 +1,8 @@
 from .service import Service, ServiceTopics, Topic
-from datetime import datetime, timedelta
-from pathlib import Path
 from random import randint
 from sigenergy2mqtt.config import Config
 from typing import Any, Awaitable, Callable, Iterable, List
 import asyncio
-import json
 import logging
 import time
 
@@ -18,24 +15,6 @@ class PVOutputStatusService(Service):
         self._generation: ServiceTopics[str, Topic] = ServiceTopics(self, True, "generation", logger)
         self._temperature: ServiceTopics[str, Topic] = ServiceTopics(self, True if Config.pvoutput.temperature_topic else False, "temperature", logger)
         self._voltage: ServiceTopics[str, Topic] = ServiceTopics(self, True, "voltage", logger)
-
-        self._previous_payload: dict[str, any] = {}
-        self._persistent_state_file = Path(Config.persistent_state_path, f"pvoutput_status_{plant_index}.payload")
-        if self._persistent_state_file.is_file():
-            fmt = time.localtime(self._persistent_state_file.stat().st_mtime)
-            now = time.localtime()
-            if fmt.tm_year == now.tm_year and fmt.tm_mon == now.tm_mon and fmt.tm_mday == now.tm_mday:
-                with self._persistent_state_file.open("r") as f:
-                    try:
-                        self._previous_payload = json.load(f)
-                        self.logger.info(f"{self.__class__.__name__} - Loaded {self._persistent_state_file}")
-                    except ValueError as error:
-                        self.logger.warning(f"{self.__class__.__name__} - Failed to read {self._persistent_state_file}: {error}")
-            else:
-                self.logger.info(f"{self.__class__.__name__} - Ignored {self._persistent_state_file} because it is stale ({fmt})")
-                self._persistent_state_file.unlink(missing_ok=True)
-        else:
-            self.logger.debug(f"{self.__class__.__name__} - Persistent state file {self._persistent_state_file} not found")
 
     # region Registrations
 
@@ -81,33 +60,17 @@ class PVOutputStatusService(Service):
             now = time.localtime()
             self.logger.debug(f"{self.__class__.__name__} - Creating payload...")
             payload = {"d": time.strftime("%Y%m%d", now), "t": time.strftime("%H:%M", now), "c1": 1}
-            if "t" in self._previous_payload:
-                t1 = datetime.strptime(self._previous_payload["t"], "%H:%M")
-                t2 = datetime.strptime(payload["t"], "%H:%M")
-                if t2 < t1:
-                    t2 += timedelta(days=1)
-                delta = t2 - t1
-                hours = delta.total_seconds() / 3600
-            else:
-                hours = None
             async with self.lock(timeout=5):
                 if self._generation.enabled:
                     payload["v1"], _ = self._generation.sum()
-                    if hours and "v1" in self._previous_payload:
-                        payload["v2"] = round((payload["v1"] - self._previous_payload["v1"]) / hours)
                 if self._consumption.enabled:
                     payload["v3"], _ = self._consumption.sum()
-                    if hours and "v3" in self._previous_payload:
-                        payload["v4"] = round((payload["v3"] - self._previous_payload["v3"]) / hours)
                 if self._temperature.enabled:
                     payload["v5"], _ = self._temperature.average(1)
                 if self._voltage.enabled:
                     payload["v6"], _ = self._voltage.average(1)
             if payload["v1"] or payload["v3"]:  # At least one of the values v1, v2, v3 or v4 must be present
                 await self.upload_payload("https://pvoutput.org/service/r2/addstatus.jsp", payload)
-                self._previous_payload = payload
-                with self._persistent_state_file.open("w") as f:
-                    json.dump(payload, f)
             else:
                 self.logger.warning(f"{self.__class__.__name__} - No generation or consumption data to upload, skipping...")
 
