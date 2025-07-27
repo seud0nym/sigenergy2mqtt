b2d97c15759cbe9cd839a4e6f60e5cae56e3b0d0
Stephen Stevens <seud0nym@yahoo.com.au>
2025-07-27 16:22:44 +1000

    Added get_waiter_count()

--- sigenergy2mqtt/modbus/lock_factory.py
+++ sigenergy2mqtt/modbus/lock_factory.py
@@ -3,16 +3,22 @@ from pymodbus.client import AsyncModbusTcpClient as ModbusClient
 import logging
 
 
-
 class ModbusLockFactory:
     _locks: dict[ModbusClient, ModbusLock] = {}
     _logger = logging.getLogger("pymodbus")
 
     @classmethod
-    def get_lock(self, modbus: ModbusClient) -> ModbusLock:
-        if modbus not in self._locks:
-            self._locks[modbus] = ModbusLock(modbus)
-        lock = self._locks[modbus]
+    def get_lock(cls, modbus: ModbusClient) -> ModbusLock:
+        if modbus not in cls._locks:
+            cls._locks[modbus] = ModbusLock(modbus)
+        lock = cls._locks[modbus]
         if lock.waiters > 5:
-            self._logger.debug(f"Lock on {lock.host} has {lock.waiters} waiters")
+            cls._logger.debug(f"Lock on {lock.host} has {lock.waiters} waiters")
         return lock
+
+    @classmethod
+    def get_waiter_count(cls) -> int:
+        waiters: int = 0
+        for lock in cls._locks.values():
+            waiters += lock.waiters
+        return waiters
