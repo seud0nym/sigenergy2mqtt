4559ac897543a38abb96691ce84ef01f0de5ac7b
Stephen Stevens <seud0nym@yahoo.com.au>
2025-09-28 17:28:27 +1000

    Added docker compose testing option

--- test/docker-test.bash
+++ test/docker-test.bash
@@ -5,11 +5,16 @@ cd "$(cd $(dirname $0)/..; pwd)"
 python3 -m build || exit
 python3 -m twine check dist/* || exit
 docker buildx build --platform linux/amd64 --load -t sigenergy2mqtt:latest . || exit
-docker run -it \
-    -e TZ=Australia/Melbourne \
-    -e SIGENERGY2MQTT_LOG_LEVEL=DEBUG \
-    -e SIGENERGY2MQTT_MODBUS_AUTO_DISCOVERY=once \
-    -v /var/lib/sigenergy2mqtt:/data \
-    --network host \
-    sigenergy2mqtt:latest
 
+if [[ $1 == compose ]]; then
+    cd test
+    docker compose up
+else
+    docker run -it \
+        -e TZ=Australia/Melbourne \
+        -e SIGENERGY2MQTT_LOG_LEVEL=DEBUG \
+        -e SIGENERGY2MQTT_MODBUS_AUTO_DISCOVERY=once \
+        -v /var/lib/sigenergy2mqtt:/data \
+        --network host \
+        sigenergy2mqtt:latest
+fi
