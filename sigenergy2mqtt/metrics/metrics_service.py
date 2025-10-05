af1f58daa7a7d7e82c3d9723472565ad3586db85
Stephen Stevens <seud0nym@yahoo.com.au>
2025-10-05 13:49:18 +1100

    Added default_entity_id in MQTT discovery for HA Core 2025.10

--- sigenergy2mqtt/metrics/metrics_service.py
+++ sigenergy2mqtt/metrics/metrics_service.py
@@ -27,6 +27,7 @@ class MetricsService(Device):
             "sigenergy2mqtt_modbus_reads_sec": {
                 "platform": "sensor",
                 "name": "Modbus Reads/second",
+                "default_entity_id": "sigenergy2mqtt_modbus_reads_sec",
                 "object_id": "sigenergy2mqtt_modbus_reads_sec",
                 "unique_id": "sigenergy2mqtt_modbus_reads_sec",
                 "icon": "mdi:timer-play-outline",
@@ -36,6 +37,7 @@ class MetricsService(Device):
             "sigenergy2mqtt_modbus_read_errors": {
                 "platform": "sensor",
                 "name": "Modbus Read Errors",
+                "default_entity_id": "sigenergy2mqtt_modbus_read_errors",
                 "object_id": "sigenergy2mqtt_modbus_read_errors",
                 "unique_id": "sigenergy2mqtt_modbus_read_errors",
                 "icon": "mdi:counter",
@@ -45,6 +47,7 @@ class MetricsService(Device):
             "sigenergy2mqtt_modbus_read_max": {
                 "platform": "sensor",
                 "name": "Modbus Read Max",
+                "default_entity_id": "sigenergy2mqtt_modbus_read_max",
                 "object_id": "sigenergy2mqtt_modbus_read_max",
                 "unique_id": "sigenergy2mqtt_modbus_read_max",
                 "icon": "mdi:timer-plus-outline",
@@ -56,6 +59,7 @@ class MetricsService(Device):
             "sigenergy2mqtt_modbus_read_mean": {
                 "platform": "sensor",
                 "name": "Modbus Read Mean",
+                "default_entity_id": "sigenergy2mqtt_modbus_read_mean",
                 "object_id": "sigenergy2mqtt_modbus_read_mean",
                 "unique_id": "sigenergy2mqtt_modbus_read_mean",
                 "icon": "mdi:timer-outline",
@@ -67,6 +71,7 @@ class MetricsService(Device):
             "sigenergy2mqtt_modbus_read_min": {
                 "platform": "sensor",
                 "name": "Modbus Read Min",
+                "default_entity_id": "sigenergy2mqtt_modbus_read_min",
                 "object_id": "sigenergy2mqtt_modbus_read_min",
                 "unique_id": "sigenergy2mqtt_modbus_read_min",
                 "icon": "mdi:timer-minus-outline",
@@ -78,6 +83,7 @@ class MetricsService(Device):
             "sigenergy2mqtt_modbus_write_errors": {
                 "platform": "sensor",
                 "name": "Modbus Write Errors",
+                "default_entity_id": "sigenergy2mqtt_modbus_write_errors",
                 "object_id": "sigenergy2mqtt_modbus_write_errors",
                 "unique_id": "sigenergy2mqtt_modbus_write_errors",
                 "icon": "mdi:counter",
@@ -87,6 +93,7 @@ class MetricsService(Device):
             "sigenergy2mqtt_modbus_write_max": {
                 "platform": "sensor",
                 "name": "Modbus Write Max",
+                "default_entity_id": "sigenergy2mqtt_modbus_write_max",
                 "object_id": "sigenergy2mqtt_modbus_write_max",
                 "unique_id": "sigenergy2mqtt_modbus_write_max",
                 "icon": "mdi:timer-plus-outline",
@@ -98,6 +105,7 @@ class MetricsService(Device):
             "sigenergy2mqtt_modbus_write_mean": {
                 "platform": "sensor",
                 "name": "Modbus Write Mean",
+                "default_entity_id": "sigenergy2mqtt_modbus_write_mean",
                 "object_id": "sigenergy2mqtt_modbus_write_mean",
                 "unique_id": "sigenergy2mqtt_modbus_write_mean",
                 "icon": "mdi:timer-outline",
@@ -109,6 +117,7 @@ class MetricsService(Device):
             "sigenergy2mqtt_modbus_write_min": {
                 "platform": "sensor",
                 "name": "Modbus Write Min",
+                "default_entity_id": "sigenergy2mqtt_modbus_write_min",
                 "object_id": "sigenergy2mqtt_modbus_write_min",
                 "unique_id": "sigenergy2mqtt_modbus_write_min",
                 "icon": "mdi:timer-minus-outline",
@@ -120,6 +129,7 @@ class MetricsService(Device):
             "sigenergy2mqtt_modbus_locks": {
                 "platform": "sensor",
                 "name": "Modbus Active Locks",
+                "default_entity_id": "sigenergy2mqtt_modbus_locks",
                 "object_id": "sigenergy2mqtt_modbus_locks",
                 "unique_id": "sigenergy2mqtt_modbus_locks",
                 "icon": "mdi:eye-lock",
@@ -129,6 +139,7 @@ class MetricsService(Device):
             "sigenergy2mqtt_started": {
                 "platform": "sensor",
                 "name": "Started",
+                "default_entity_id": "sigenergy2mqtt_started",
                 "object_id": "sigenergy2mqtt_started",
                 "unique_id": "sigenergy2mqtt_started",
                 "device_class": "timestamp",
@@ -140,6 +151,7 @@ class MetricsService(Device):
             "sigenergy2mqtt_modbus_protocol": {
                 "platform": "sensor",
                 "name": "Protocol Version",
+                "default_entity_id": "sigenergy2mqtt_modbus_protocol",
                 "object_id": "sigenergy2mqtt_modbus_protocol",
                 "unique_id": "sigenergy2mqtt_modbus_protocol",
                 "entity_category": "diagnostic",
@@ -149,6 +161,7 @@ class MetricsService(Device):
             "sigenergy2mqtt_modbus_protocol_published": {
                 "platform": "sensor",
                 "name": "Protocol Published",
+                "default_entity_id": "sigenergy2mqtt_modbus_protocol_published",
                 "object_id": "sigenergy2mqtt_modbus_protocol_published",
                 "unique_id": "sigenergy2mqtt_modbus_protocol_published",
                 "entity_category": "diagnostic",
