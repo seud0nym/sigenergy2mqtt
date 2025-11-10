b129cfa47422bb5b40106164faa7c337c1639db5
Stephen Stevens <seud0nym@yahoo.com.au>
2025-11-11 09:57:57 +1100

    Improved verification of end of day updates

--- sigenergy2mqtt/pvoutput/output.py
+++ sigenergy2mqtt/pvoutput/output.py
@@ -20,11 +20,11 @@ class PVOutputOutputService(Service):
         self._latest_peak_at: str = None
         self._previous_payload: dict = None
         self._service_topics: dict[str, ServiceTopics] = {
-            OutputField.GENERATION: ServiceTopics(self, False, logger, value_key=OutputField.GENERATION),
+            OutputField.GENERATION: ServiceTopics(self, False, logger, value_key=OutputField.GENERATION),  # Disable EoD generation update because it is updated via the status service
+            OutputField.IMPORTS: ServiceTopics(self, Config.pvoutput.imports, logger, value_key=OutputField.IMPORTS),
             OutputField.EXPORTS: ServiceTopics(self, Config.pvoutput.exports, logger, value_key=OutputField.EXPORTS),
             OutputField.PEAK_POWER: ServiceTopics(self, True, logger, value_key=OutputField.PEAK_POWER, datetime_key="pt", calculation=Calculation.SUM | Calculation.PEAK),
-            OutputField.IMPORTS: ServiceTopics(self, Config.pvoutput.imports, logger, value_key=OutputField.IMPORTS),
-            OutputField.CONSUMPTION: ServiceTopics(self, Config.pvoutput.consumption_enabled, logger, value_key=OutputField.CONSUMPTION),
+            OutputField.CONSUMPTION: ServiceTopics(self, False, logger, value_key=OutputField.CONSUMPTION),  # Disable EoD consumption update because it is updated via the status service
         }
         for field, topic_list in topics.items():
             if field in self._service_topics:
@@ -47,7 +47,7 @@ class PVOutputOutputService(Service):
         now = time.mktime(t)
         if Config.pvoutput.output_hour == -1:  # Update at status interval
             interval, _ = await self.seconds_until_status_upload(rand_min=16, rand_max=30)
-            next = now + interval
+            next = now + 120 + interval  # Wait 2 minutes plus the interval to ensure status upload has completed
         else:
             if Config.pvoutput.testing:
                 next = now + 60
@@ -57,78 +57,100 @@ class PVOutputOutputService(Service):
                     today = datetime.fromtimestamp(next)
                     tomorrow = today + timedelta(days=1, seconds=randint(-15, 15))  # Add a random offset of up to 15 seconds for variability
                     next = tomorrow.timestamp()
-            self.logger.debug(f"{self.__class__.__name__} Next update at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(next))} ({next - now}s)")
         return next
 
-    async def _upload_and_verify(self, payload: dict[str, int | str], bypass_verify: bool = False) -> None:
-        for _ in range(1, 6, 1):
-            if await self.upload_payload("https://pvoutput.org/service/r2/addoutput.jsp", payload):
-                if bypass_verify:
+    async def _upload(self, payload: dict[str, int | str], last_upload_of_day: bool = False) -> None:
+        upload_retries: int = 5 if last_upload_of_day else 2
+        changed: bool = self._is_payload_changed(payload)
+        matches: bool = None
+        for attempt in range(1, upload_retries + 1, 1):
+            if changed:
+                uploaded = await self.upload_payload("https://pvoutput.org/service/r2/addoutput.jsp", payload)
+            else:
+                uploaded = None
+                self.logger.info(f"{self.__class__.__name__} Skipped uploading unchanged {payload=}")
+            if last_upload_of_day or not changed:
+                matches = await self._verify(payload, force=last_upload_of_day)
+                if matches:
                     break
                 else:
-                    self.logger.debug(f"{self.__class__.__name__} Verifying uploaded {payload=}")
-                    url = f"https://pvoutput.org/service/r2/getoutput.jsp?df={payload['d']}&dt={payload['d']}"
-                    for validate in range(1, 6, 1):
-                        matches = True
-                        self.logger.debug(f"{self.__class__.__name__} Waiting for 60s before checking that the upload has been processed successfully...")
-                        await asyncio.sleep(0.1 if Config.pvoutput.testing else 60)
-                        try:
-                            if Config.pvoutput.testing:
-                                self.logger.debug(f"{self.__class__.__name__} Verification attempt #{validate} simulation for testing mode, not sending request to {url=}")
-                                if validate > 1:
-                                    v = re.split(
-                                        r"[,]",
-                                        f"{payload['d']},{payload.get('g', 'NaN')},{payload.get('c', 'NaN')},{payload.get('e', 'NaN')},0,{payload.get('pp', 'NaN')},{payload.get('pt', '')},Showers,12,16,{payload.get('ip', 'NaN')},0,0,0",
-                                    )
-                                else:
-                                    self.logger.debug(f"{self.__class__.__name__} Verification attempt #{validate} simulation FAILED")
-                                    matches = False
-                            else:
-                                self.logger.debug(f"{self.__class__.__name__} Verification attempt #{validate} to {url=}...")
-                                with requests.get(url, headers=self.request_headers, timeout=10) as response:
-                                    limit, remaining, at, reset = self.get_response_headers(response)
-                                    if response.status_code == 200:
-                                        self.logger.debug(
-                                            f"{self.__class__.__name__} Verification attempt #{validate} OKAY status_code={response.status_code} {limit=} {remaining=} reset={time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(at))} ({reset}s) response={response.text}"
-                                        )
-                                        v = re.split(r"[,]", response.text)
-                                    else:
-                                        self.logger.debug(f"{self.__class__.__name__} Verification attempt #{validate} FAILED status_code={response.status_code} reason={response.reason}")
-                                        matches = False
-                            if matches:
-                                result = {}
-                                result["d"] = v[0]
-                                result["g"] = int(v[1]) if len(v) > 1 and v[1] != "NaN" else None
-                                result["e"] = int(v[3]) if len(v) > 3 and v[3] != "NaN" else None
-                                result["pp"] = int(v[5]) if len(v) > 5 and v[5] != "NaN" else None
-                                result["ip"] = int(v[10]) if len(v) > 10 and v[10] != "NaN" else None
-                                for topic in [t for t in self._service_topics.values() if t.enabled]:
-                                    if topic._value_key in payload and topic._value_key in result:
-                                        if payload[topic._value_key] == result[topic._value_key]:
-                                            self.logger.debug(
-                                                f"{self.__class__.__name__} Verified payload['{topic._value_key}']={payload[topic._value_key]} == result['{topic._value_key}']={result[topic._value_key]}"
-                                            )
-                                        else:
-                                            self.logger.debug(
-                                                f"{self.__class__.__name__} Verification failure: payload['{topic._value_key}']={payload[topic._value_key]} != result['{topic._value_key}']={result[topic._value_key]}"
-                                            )
-                                            matches = False
-                                            break
-                            if matches:
-                                self.logger.debug(f"{self.__class__.__name__} Verified uploaded {payload=}")
-                                break
-                            elif validate < 5:
-                                self.logger.debug(f"{self.__class__.__name__} Verification attempt #{validate} FAILED, retrying...")
-                        except requests.exceptions.HTTPError as exc:
-                            self.logger.error(f"{self.__class__.__name__} HTTP Error: {exc}")
-                        except requests.exceptions.ConnectionError as exc:
-                            self.logger.error(f"{self.__class__.__name__} Error Connecting: {exc}")
-                        except requests.exceptions.Timeout as exc:
-                            self.logger.error(f"{self.__class__.__name__} Timeout Error: {exc}")
-                        except Exception as exc:
-                            self.logger.error(f"{self.__class__.__name__} {exc}")
-                    if matches:
+                    changed = True  # Force re-upload if verification failed
+                    if last_upload_of_day and attempt > 1:
                         break
+            elif uploaded or not changed:
+                break
+        self.logger.debug(f"{self.__class__.__name__} Upload completed for {payload=}: {changed=} {uploaded=} attempts={attempt} verified={matches} ({last_upload_of_day=})")
+        if changed and Config.pvoutput.output_hour == -1:
+            self._previous_payload = payload
+
+    async def _verify(self, payload: dict[str, int | str], force: bool = False) -> bool:
+        self.logger.debug(f"{self.__class__.__name__} Verifying uploaded {payload=}")
+        url = f"https://pvoutput.org/service/r2/getoutput.jsp?df={payload['d']}&dt={payload['d']}"
+        verify_retries: int = 3 if force else 1
+        initial_wait: float = 0.1 if not force or Config.pvoutput.testing else 120.0
+        subsequent_wait: float = 0.1 if Config.pvoutput.testing else 60.0
+        for validate in range(1, verify_retries + 1, 1):
+            matches: bool = True
+            wait: float = initial_wait if validate == 1 else subsequent_wait
+            self.logger.debug(f"{self.__class__.__name__} Waiting for {wait}s before checking that the upload has been processed successfully...")
+            await asyncio.sleep(wait)
+            try:
+                if Config.pvoutput.testing:
+                    self.logger.debug(f"{self.__class__.__name__} Verification attempt #{validate} simulation for testing mode, not sending request to {url=}")
+                    if validate > 1:
+                        v = re.split(
+                            r"[,]",
+                            f"{payload['d']},{payload.get('g', 'NaN')},{payload.get('c', 'NaN')},{payload.get('e', 'NaN')},0,{payload.get('pp', 'NaN')},{payload.get('pt', '')},Showers,12,16,{payload.get('ip', 'NaN')},0,0,0",
+                        )
+                    else:
+                        self.logger.debug(f"{self.__class__.__name__} Verification attempt #{validate} simulation FAILED")
+                        matches = False
+                else:
+                    self.logger.debug(f"{self.__class__.__name__} Verification attempt #{validate} to {url=}...")
+                    with requests.get(url, headers=self.request_headers, timeout=10) as response:
+                        limit, remaining, at, reset = self.get_response_headers(response)
+                        if response.status_code == 200:
+                            self.logger.debug(
+                                f"{self.__class__.__name__} Verification attempt #{validate} OKAY status_code={response.status_code} {limit=} {remaining=} reset={time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(at))} ({reset}s) response={response.text}"
+                            )
+                            v = re.split(r"[,]", response.text)
+                        else:
+                            self.logger.debug(f"{self.__class__.__name__} Verification attempt #{validate} FAILED status_code={response.status_code} reason={response.reason}")
+                            matches = False
+                if matches:
+                    result = {}
+                    result["d"] = v[0]
+                    result["e"] = int(v[3]) if len(v) > 3 and v[3] != "NaN" else None
+                    result["pp"] = int(v[5]) if len(v) > 5 and v[5] != "NaN" else None
+                    result["ip"] = int(v[10]) if len(v) > 10 and v[10] != "NaN" else None
+                    for topic in [t for t in self._service_topics.values() if t.enabled]:
+                        if topic._value_key in payload and topic._value_key in result:
+                            if payload[topic._value_key] == result[topic._value_key]:
+                                self.logger.debug(
+                                    f"{self.__class__.__name__} Verified payload['{topic._value_key}']={payload[topic._value_key]} == result['{topic._value_key}']={result[topic._value_key]}"
+                                )
+                            else:
+                                self.logger.debug(
+                                    f"{self.__class__.__name__} Verification failure: payload['{topic._value_key}']={payload[topic._value_key]} != result['{topic._value_key}']={result[topic._value_key]}"
+                                )
+                                matches = False
+                                break
+                if matches:
+                    self.logger.info(f"{self.__class__.__name__} Verified uploaded {payload=} downloaded={result} ({response.text})")
+                    break
+                elif validate < verify_retries:
+                    self.logger.debug(f"{self.__class__.__name__} Verification attempt #{validate} of uploaded {payload=} FAILED, retrying...")
+                else:
+                    self.logger.error(f"{self.__class__.__name__} Verification FAILED after {validate} attempts for uploaded {payload=} downloaded={result} ({response.text})")
+            except requests.exceptions.HTTPError as exc:
+                self.logger.error(f"{self.__class__.__name__} HTTP Error: {exc}")
+            except requests.exceptions.ConnectionError as exc:
+                self.logger.error(f"{self.__class__.__name__} Error Connecting: {exc}")
+            except requests.exceptions.Timeout as exc:
+                self.logger.error(f"{self.__class__.__name__} Timeout Error: {exc}")
+            except Exception as exc:
+                self.logger.error(f"{self.__class__.__name__} {exc}")
+        return matches
 
     def schedule(self, modbus: Any, mqtt: MqttClient) -> List[Callable[[Any, MqttClient, Iterable[Any]], Awaitable[None]]]:
         async def publish_updates(modbus: Any, mqtt: MqttClient, *sensors: Any) -> None:
@@ -147,14 +169,10 @@ class PVOutputOutputService(Service):
                             for topic in [t for t in self._service_topics.values() if t.enabled]:
                                 topic.add_to_payload(payload, interval, now_struct)
                         tomorrow = await self._next_output_upload(minute)
-                        bypass_verify: bool = time.localtime(tomorrow).tm_yday == now_struct.tm_yday  # Bypass verification except on last upload of the day
-                        if self._is_payload_changed(payload) or not bypass_verify:  # Only upload if changed or verification required
-                            await self._upload_and_verify(payload, bypass_verify)
-                            if Config.pvoutput.output_hour == -1:
-                                self._previous_payload = payload
-                        else:
-                            self.logger.info(f"{self.__class__.__name__} SKIPPED uploading {payload=} - Unchanged from last update")
+                        last_update_of_day: bool = time.localtime(tomorrow).tm_yday != now_struct.tm_yday  # Bypass verification except on last upload of the day
+                        await self._upload(payload, last_update_of_day)
                         next = tomorrow
+                        self.logger.debug(f"{self.__class__.__name__} Next update at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(next))} ({next - now:.2f}s)")
                     elif int(now) % (30 if Config.pvoutput.testing else 900) == 0:
                         for topic in [t for k, t in self._service_topics.items() if t.enabled and k != OutputField.PEAK_POWER]:
                             topic.check_is_updating(5, now_struct)
@@ -162,7 +180,6 @@ class PVOutputOutputService(Service):
                         if total is not None and total > 0 and self._latest_peak_at != at:
                             self._latest_peak_at = at
                             self._logger.info(f"{self.__class__.__name__} Peak Power {total:.0f}W recorded at {at}")
-                        self.logger.debug(f"{self.__class__.__name__} Next update at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(next))} ({next - now}s)")
                     if last:
                         was = time.localtime(last)
                         if was.tm_yday != now_struct.tm_yday:
