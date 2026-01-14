45fc6556a7498ec67ade336a8b0c2ef7c001a433
Stephen Stevens <seud0nym@yahoo.com.au>
2026-01-14 15:36:51 +1100

    Unused import

--- sigenergy2mqtt/sensors/sanity_check.py
+++ sigenergy2mqtt/sensors/sanity_check.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import TYPE_CHECKING
 
 from sigenergy2mqtt.config import Config
 from sigenergy2mqtt.modbus.types import ModbusDataType
