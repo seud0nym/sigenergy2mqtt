0865284ecb94ed606104b1f67f45702c4ea74d86
Stephen Stevens <seud0nym@yahoo.com.au>
2026-02-01 17:14:28 +1100

    Missing main function

--- sigenergy2mqtt/__main__.py
+++ sigenergy2mqtt/__main__.py
@@ -8,3 +8,7 @@ from sigenergy2mqtt.main import async_main
 def main():
     initialize()
     asyncio.run(async_main(), debug=True if Config.log_level == logging.DEBUG else False)
+
+
+if __name__ == "__main__":
+    main()
