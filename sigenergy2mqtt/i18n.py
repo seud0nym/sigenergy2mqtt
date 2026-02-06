5526cb796c8b369e65a274ac70a0cd6248811d70
Stephen Stevens <seud0nym@yahoo.com.au>
2026-02-06 20:24:07 +1100

    Fixed return value when not translating

--- sigenergy2mqtt/i18n.py
+++ sigenergy2mqtt/i18n.py
@@ -125,7 +125,7 @@ def load(language: str):
 
 def _t(key: str, default: str | None = None, debugging: bool = False, **kwargs) -> str:
     if kwargs.get("translate", True) is False:
-        return key
+        return default if default is not None else key
     translation, language, translated = _translator.translate(key, default)
     if not translated:
         if debugging:
