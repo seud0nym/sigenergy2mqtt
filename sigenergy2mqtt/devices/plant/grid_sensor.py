b8260885f3b57f03643da9d8ed4ecd7de145fd8b
Stephen Stevens <seud0nym@yahoo.com.au>
2026-03-05 13:44:06 +1100

    refactor: Initialize GridSensor active power and grid status as local variables within `_register_sensors` instead of instance attributes.

--- sigenergy2mqtt/devices/plant/grid_sensor.py
+++ sigenergy2mqtt/devices/plant/grid_sensor.py
@@ -14,8 +14,6 @@ class GridSensor(ModbusDevice):
     ):
         name = "Sigenergy Plant Grid Sensor" if plant_index == 0 else f"Sigenergy Plant {plant_index + 1} Grid Sensor"
         super().__init__(device_type, name, plant_index, 247, "Grid Sensor", protocol_version)
-        self.active_power = ro.GridSensorActivePower(plant_index)
-        self.grid_status = ro.GridStatus(plant_index)
 
     @classmethod
     async def create(cls, plant_index: int, device_type: DeviceType, protocol_version: Protocol, power_phases: int, consumption_group: str | None, modbus_client: ModbusClient) -> "GridSensor":
@@ -24,8 +22,12 @@ class GridSensor(ModbusDevice):
         return grid_sensor
 
     async def _register_sensors(self, power_phases: int, consumption_group: str | None, modbus_client: ModbusClient) -> None:
+        active_power = ro.GridSensorActivePower(self.plant_index)
+        grid_status = ro.GridStatus(self.plant_index)
+
+        self._add_read_sensor(grid_status)
         self._add_read_sensor(ro.GridSensorStatus(self.plant_index))
-        self._add_read_sensor(self.active_power, consumption_group)
+        self._add_read_sensor(active_power, consumption_group)
         self._add_read_sensor(ro.GridSensorReactivePower(self.plant_index))
         self._add_read_sensor(ro.GridPhaseActivePower(self.plant_index, "A"))
         self._add_read_sensor(ro.GridPhaseReactivePower(self.plant_index, "A"))
@@ -41,15 +43,15 @@ class GridSensor(ModbusDevice):
             self._add_read_sensor(ro.GridPhaseReactivePower(self.plant_index, "C"))
             self._add_read_sensor(ro.ReservedGridPhaseVoltage(self.plant_index, "C"))
             self._add_read_sensor(ro.ReservedGridPhaseCurrent(self.plant_index, "C"))
-        self._add_read_sensor(self.grid_status)
 
-        export_power = derived.GridSensorExportPower(self.plant_index, self.active_power)
-        import_power = derived.GridSensorImportPower(self.plant_index, self.active_power)
-        self._add_derived_sensor(export_power, self.active_power)
-        self._add_derived_sensor(import_power, self.active_power)
-        export_energy = ro.PlantTotalExportedEnergy(self.plant_index)
-        self._add_read_sensor(export_energy)
-        self._add_derived_sensor(derived.GridSensorDailyExportEnergy(self.plant_index, export_energy), export_energy)
         import_energy = ro.PlantTotalImportedEnergy(self.plant_index)
+        export_energy = ro.PlantTotalExportedEnergy(self.plant_index)
         self._add_read_sensor(import_energy)
+        self._add_read_sensor(export_energy)
+
+        import_power = derived.GridSensorImportPower(self.plant_index, active_power)
+        export_power = derived.GridSensorExportPower(self.plant_index, active_power)
+        self._add_derived_sensor(import_power, active_power)
+        self._add_derived_sensor(export_power, active_power)
+        self._add_derived_sensor(derived.GridSensorDailyExportEnergy(self.plant_index, export_energy), export_energy)
         self._add_derived_sensor(derived.GridSensorDailyImportEnergy(self.plant_index, import_energy), import_energy)
