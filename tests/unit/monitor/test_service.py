c74e4d1185f3bdddf9cdcb8fa92fac9baf0c79fb
Stephen Stevens <seud0nym@yahoo.com.au>
2026-09-14 17:59:48 +1000

    refactor: include V1HttpWriter in InfluxBase import

--- tests/unit/monitor/test_service.py
+++ tests/unit/monitor/test_service.py
@@ -8,7 +8,7 @@ import requests
 
 from sigenergy2mqtt.common import service_health_registry
 from sigenergy2mqtt.config import active_config
-from sigenergy2mqtt.influxdb.base import InfluxBase
+from sigenergy2mqtt.influxdb.base import InfluxBase, V1HttpWriter
 from sigenergy2mqtt.modbus import ModbusClientFactory
 from sigenergy2mqtt.monitor.service import MonitorService
 from sigenergy2mqtt.pvoutput.service import Service as PvOutputService
